73acd9b57498667696080528f0558703141edeac
Eduardohiposa <hipocremesa@gmail.com>
2026-08-11 11:30:56 -0300

    mudanças novo web

---
+++ Novo Web/novo web/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home(){
+
+return(
+<>
+<h1>Sobre</h1>
+<p>Sistema desenvolvido em React.</p>
+</>
+)
+
+}
+export default Home
---
+++ Novo Web/novo web/src/pages/Logar.jsx
@@ -0,0 +1,15 @@
+function Logar(){
+
+
+
+    
+return(
+<>
+<h1>Sobre</h1>
+<p>Sistema desenvolvido em React.</p>
+</>
+)
+
+}
+export default Logar
+
